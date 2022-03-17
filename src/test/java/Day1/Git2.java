fcbe7f47209798d97409cbc29891e763edeef4bc
atamyradov <atamyradov2018@gmail.com>
2022-03-17 20:07:05 +0100

    Git 2 and Git3 files updated

--- src/test/java/Day1/Git2.java
+++ src/test/java/Day1/Git2.java
@@ -4,4 +4,5 @@ public class Git2 {
 
     //Git2 eklenmedi!! ama degisiklik yaptim
     //modifiye edildi!!!
+    //commit sonrasi git 2 degisti!!
 }
--- src/test/java/Day1/Git3.java
+++ src/test/java/Day1/Git3.java
@@ -3,4 +3,6 @@ package Day1;
 public class Git3 {
 
     //git3 olusturuldu
+
+//commit sonrasi git3 degisti!!
 }
